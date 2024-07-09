ac9f7f61549f516936095e673667bd19461746a4
HarshmeetUNSW <166305815+HarshmeetUNSW@users.noreply.github.com>
2024-07-09 06:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with col2:
     st.title('Hate Speech Detector')
 
 # Load the model and tokenizer once
-@st.experimental_singleton
+@st.cache_resource 
 def load_model():
     model = BertForSequenceClassification.from_pretrained('final_fine_tuned_bert/')
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
